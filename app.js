ac6d7b6a2df2864363ce65911dfc7b5c208fd515
Dan Toohey <100450914+2e-D@users.noreply.github.com>
2022-10-22 21:28:57 +0100

    JS for the landing page
    JavaScript features include
    - Background changes color when button clicked (cycles through an array)
    - Greeting Message changes depending on time of day (uses if Statement)

--- app.js
+++ app.js
@@ -1,1 +1,50 @@
-h
+const output = document.querySelector(".output");
+const userName = document.querySelector('input[name="userName"]');
+
+const btn = document.querySelector("button");
+
+btn.onclick = () => {
+  if (userName.value.length > 1) {
+    const message = `Hello ${userName.value} 👋 Welcome to my Portfolio Website`;
+
+    output.textContent = message;
+  } else {
+    output.textContent = "Please enter more than 1 character";
+
+    userName.style.borderColor = "red";
+  }
+};
+
+let currentTime2 = new Date().getHours();
+if (4 <= currentTime2 && currentTime2 < 12) {
+  document.getElementById("greet").innerHTML = "Good Morning";
+} else if (12 <= currentTime2 && currentTime2 < 17) {
+  document.getElementById("greet").innerHTML = "Good Afternoon";
+} else {
+  document.getElementById("greet").innerHTML = "Good Evening";
+}
+
+const colors = [
+  "Green",
+  "Red",
+  "Cyan",
+  "Yellow",
+  "Orange",
+  "Pink",
+  "Purple",
+  "Light Blue",
+  "Dark Blue",
+  "Blue",
+];
+
+const btnColor = document.getElementById("Change");
+
+btnColor.addEventListener("click", function () {
+  const randomNumber = getRandomNumber();
+
+  document.body.style.backgroundColor = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
